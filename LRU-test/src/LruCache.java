f2be66e08392a0a75177700422e32697733bc2ec
Panamec1 <283091@niuitmo.ru>
2022-11-06 12:53:37 +0300

    Checked

--- LRU-test/src/LruCache.java
+++ LRU-test/src/LruCache.java
@@ -4,56 +4,72 @@ import java.util.List;
 import java.util.Map;
 
 public class LruCache<Key, Val> {
-
-    private final int maxSize            = 100;
-    private       int size               = 0;
-    private final Map<Key, Val> kvmapper = new HashMap<>();
+    private final Map<Key, Val> kvmapper  = new HashMap<>();
     private final List<Key> klist         = new LinkedList<>();
-    private       int sizer              = maxSize;
+    private       int sizer               = 100;
 
 
     public LruCache(){}
 
     LruCache(int size) {
-        assert (size >= 0 && size <= maxSize);
+        assert (size >= 0):"Set size cannot be negative";
+        assert (size <= sizer):"Set size is to big";
         sizer = size;
     }
 
     public int getCacheSize() {
+        int size = klist.size();
+        assert (size == klist.size()):"Size must not change";
         return size;
     }
 
 
     public Val get(Key key) {
-        assert (kvmapper.containsKey(key));
-        return kvmapper.get(key);
+        assert (key != null):"Key cannot be null";
+        assert (kvmapper.containsKey(key)):"There's no key";
+
+        int si    = klist.size();
+        Val value = kvmapper.get(key);
+
+        assert (value == kvmapper.get(key)):"Value didn't change";
+        assert (si == klist.size()):"Size must not change";
+        return value;
     }
 
     public Key getTopKey() {
-        assert (size != 0);
-        return klist.get(klist.size() - 1);
+        assert (klist.size()  != 0):"There is no elements in Cache";
+
+        int size = klist.size();
+        Key key  = klist.get(klist.size() - 1);
+
+
+        assert (key  == klist.get(klist.size() - 1)):"Top key must not change";
+        assert (size == klist.size()):"Size must not change";
+        return key;
     }
 
     public void add(Key key, Val value) {
-        size = size + 1;
+        assert (key   != null):"Add key cannot be null";
+        assert (value != null):"Add value cannot be null";
 
-        if (sizer == klist.size() ) {
-            size = size - 1;
+        if (sizer == klist.size()) {
             Key oldest = klist.get(0);
             klist.remove(oldest);
             kvmapper.remove(oldest);
-
-
         }
-
         if (kvmapper.containsKey(key)) {
-            size = size - 1;
             kvmapper.replace(key, value);
             klist.remove(key);
             klist.add(key);
+            assert (kvmapper.get(key) == value):"Inserted wrong value with for the key";
+            assert (klist.size() <= sizer):"Add key cannot be null";
             return;
         }
         kvmapper.put(key, value);
         klist.add(key);
+
+        assert (klist.size() <= sizer):"Add key cannot be null";
+        assert (kvmapper.get(key) == value):"Inserted wrong value with for the key";
+        assert (kvmapper.containsKey(key)):"There's no key";
     }
 }
--- LRU-test/src/LruCacheTest.java
+++ LRU-test/src/LruCacheTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TestForLruCache {
+class LruCacheTest {
 
     @Test
     public void NotExistsKey() {
