9eca682fb8e224bc53acddbd684b6eae3ea3fcb7
Panamec1 <283091@niuitmo.ru>
2022-11-06 16:53:58 +0300

    Checked

--- LRU-test/src/LruCache.java
+++ LRU-test/src/LruCache.java
@@ -1,75 +1,127 @@
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 public class LruCache<Key, Val> {
-    private final Map<Key, Val> kvmapper  = new HashMap<>();
-    private final List<Key> klist         = new LinkedList<>();
-    private       int sizer               = 100;
+    public int maxSize;
+
+    private LruNode headNode = new LruNode(null, null);
+    private LruNode tailNode = new LruNode(null, null);
+    private final Map<Key, LruNode> kvmapper;
+
+    private int sizer = 100;
 
 
-    public LruCache(){}
 
-    LruCache(int size) {
-        assert (size >= 0):"Set size cannot be negative";
-        assert (size <= sizer):"Set size is to big";
-        sizer = size;
+    public LruCache() {
+        headNode.prev = tailNode;
+        tailNode.next = headNode;
+
+        maxSize = sizer;
+        kvmapper = new HashMap<>(sizer);
     }
 
-    public int getCacheSize() {
-        int size = klist.size();
-        assert (size == klist.size()):"Size must not change";
-        return size;
+
+    public LruCache(int setSize) {
+        assert (setSize >= 0) : "Set size cannot be negative";
+        assert (setSize <= sizer) : "Set size is to big";
+
+        headNode.prev = tailNode;
+        tailNode.next = headNode;
+
+        maxSize = setSize;
+        kvmapper = new HashMap<>(setSize);
     }
 
+    public Val getValueForKey(Key key) {
+        assert (key != null) : "Key cannot be null";
+        assert (kvmapper.containsKey(key)) : "There's no key";
 
-    public Val get(Key key) {
-        assert (key != null):"Key cannot be null";
-        assert (kvmapper.containsKey(key)):"There's no key";
+        int sizeOld = size();
+        LruNode dummy = new LruNode(null, null);
+        LruNode node = kvmapper.getOrDefault(key, dummy);
 
-        int si    = klist.size();
-        Val value = kvmapper.get(key);
+        // Переподвешивание изпользованной вершины
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
 
-        assert (value == kvmapper.get(key)):"Value didn't change";
-        assert (si == klist.size()):"Size must not change";
-        return value;
+        node.next = headNode;
+        node.prev = headNode.prev;
+        headNode.prev.next = node;
+        headNode.prev = node;
+
+        assert (sizeOld == size()) : "Size must not change";
+        assert (node.val == headNode.prev.val) : "Value wasn't inserted";
+        return node.val;
     }
 
     public Key getTopKey() {
-        assert (klist.size()  != 0):"There is no elements in Cache";
+        assert (size() != 0) : "There is no elements in Cache";
 
-        int size = klist.size();
-        Key key  = klist.get(klist.size() - 1);
+        int size = size();
+        Key key = headNode.prev.key;
 
-
-        assert (key  == klist.get(klist.size() - 1)):"Top key must not change";
-        assert (size == klist.size()):"Size must not change";
+        assert (size == size()) : "Size must not change";
+        assert (key == headNode.prev.key) : "Top key must not change";
         return key;
     }
 
+
     public void add(Key key, Val value) {
-        assert (key   != null):"Add key cannot be null";
-        assert (value != null):"Add value cannot be null";
+        assert (key != null) : "Add key cannot be null";
+        assert (value != null) : "Add value cannot be null";
+        assert (kvmapper.size() <= maxSize) : "Cache size is too big";
 
-        if (sizer == klist.size()) {
-            Key oldest = klist.get(0);
-            klist.remove(oldest);
-            kvmapper.remove(oldest);
-        }
-        if (kvmapper.containsKey(key)) {
-            kvmapper.replace(key, value);
-            klist.remove(key);
-            klist.add(key);
-            assert (kvmapper.get(key) == value):"Inserted wrong value with for the key";
-            assert (klist.size() <= sizer):"Add key cannot be null";
-            return;
-        }
-        kvmapper.put(key, value);
-        klist.add(key);
 
-        assert (klist.size() <= sizer):"Add key cannot be null";
-        assert (kvmapper.get(key) == value):"Inserted wrong value with for the key";
-        assert (kvmapper.containsKey(key)):"There's no key";
+        LruNode node = kvmapper.get(key);
+
+        if (kvmapper.size() == maxSize) {
+            // Удалиение последнего хвоста
+            Key tailKey = tailNode.next.key;
+            tailNode.next.next.prev = tailNode;
+            tailNode.next = tailNode.next.next;
+            kvmapper.remove(tailKey);
+        }
+
+        if (node != null) {
+            // Подвешивание новой вершины
+            node.prev = headNode.prev;
+            node.next = headNode;
+
+            // Переподвешивание вершины
+            headNode.prev.next = node;
+            headNode.prev = node;
+
+        } else {
+            // Подвешивание новой вершины
+            LruNode newNode = new LruNode(key, value);
+            newNode.prev = headNode.prev;
+            newNode.next = headNode;
+
+            // Переподвешиввание вершин
+            headNode.prev.next = newNode;
+            headNode.prev = newNode;
+
+            kvmapper.put(key, newNode);
+        }
+
+        assert (size() <= sizer) : "Add key cannot be null";
+        assert (kvmapper.containsKey(key)) : "There's no key";
+        assert (kvmapper.get(key).val == value) : "Inserted wrong value with for the key";
+        assert (headNode.prev.key == key) : "Key wasn't inserted";
+        assert (headNode.prev.val == value) : "Value wasn't inserted";
+    }
+
+    public int size() {return kvmapper.size();}
+
+    private final class LruNode {
+        Key key;
+        Val val;
+        LruNode prev;
+        LruNode next;
+
+        public LruNode(Key key, Val val) {
+            this.key = key;
+            this.val = val;
+        }
     }
 }
--- LRU-test/src/LruCacheTest.java
+++ LRU-test/src/LruCacheTest.java
@@ -8,25 +8,18 @@ class LruCacheTest {
     public void NotExistsKey() {
         LruCache<Integer, Integer> lcache = new LruCache<>();
         lcache.add(1, 1);
-        assertThrows(AssertionError.class, () -> System.out.println(lcache.get(3)));
+        assertThrows(AssertionError.class, () -> System.out.println(lcache.getValueForKey(3)));
     }
 
     @Test
     public void AddOneCheckOne() {
         LruCache<Integer, Integer> lcache = new LruCache<>();
         lcache.add(1, 1);
-        assertEquals(lcache.getCacheSize(), 1);
-        assertEquals(lcache.get(1), 1);
+        assertEquals(lcache.size(), 1);
+        assertEquals(lcache.getValueForKey(1), 1);
     }
 
-    @Test
-    public void CheckTop() {
-        LruCache<Integer, Integer> lcache = new LruCache<>();
-        lcache.add(1, 1);
-        lcache.add(2, 2);
-        assertEquals(lcache.getCacheSize(), 2);
-        assertEquals(lcache.getTopKey(), 2);
-    }
+
 
     @Test
     public void AddFourNotSetLength() {
@@ -35,27 +28,42 @@ class LruCacheTest {
         lcache.add(2, 2);
         lcache.add(3, 3);
         lcache.add(4, 4);
-        assertEquals(lcache.getCacheSize(), 4);
-        assertEquals(lcache.get(1), 1);
-        assertEquals(lcache.get(2), 2);
-        assertEquals(lcache.get(3), 3);
-        assertEquals(lcache.get(4), 4);
+        assertEquals(lcache.size(), 4);
+        assertEquals(lcache.getValueForKey(1), 1);
+        assertEquals(lcache.getValueForKey(2), 2);
+        assertEquals(lcache.getValueForKey(3), 3);
+        assertEquals(lcache.getValueForKey(4), 4);
 
+    }
+
+    @Test
+    public void CheckTop() {
+        LruCache<Integer, Integer> lcache = new LruCache<>();
+        lcache.add(1, 1);
+        lcache.add(2, 2);
+        lcache.add(3, 3);
+        assertEquals(lcache.size(), 3);
+        assertEquals(lcache.getTopKey(), 3);
     }
 
     @Test
     public void Length2Add4() {
         LruCache<Integer, Integer> lcache = new LruCache<>(2);
+        //System.out.println(lcache.size());
         lcache.add(1, 1);
+        //System.out.println(lcache.size());
         lcache.add(2, 2);
+        //System.out.println(lcache.size());
         lcache.add(3, 3);
+        //System.out.println(lcache.size());
         lcache.add(4, 4);
+        //System.out.println(lcache.size());
         lcache.add(1, 1);
-        assertEquals(lcache.getCacheSize(), 2);
-        assertEquals(lcache.get(1), 1);
-        assertEquals(lcache.get(4), 4);
-        assertThrows(AssertionError.class, () -> System.out.println(lcache.get(3)));
-        assertThrows(AssertionError.class, () -> System.out.println(lcache.get(2)));
+        assertEquals(lcache.size(), 2);
+        assertEquals(lcache.getValueForKey(1), 1);
+        assertEquals(lcache.getValueForKey(4), 4);
+        assertThrows(AssertionError.class, () -> System.out.println(lcache.getValueForKey(2)));
+        assertThrows(AssertionError.class, () -> System.out.println(lcache.getValueForKey(3)));
     }
 
     @Test
@@ -66,18 +74,14 @@ class LruCacheTest {
         lcache.add(2, 2);
         lcache.add(4, 4);
         lcache.add(1, 1);
-        assertEquals(lcache.getCacheSize(), 4);
-        assertEquals(lcache.get(1), 1);
-        assertEquals(lcache.get(4), 4);
-        assertEquals(lcache.get(3), 3);
-        assertEquals(lcache.get(2), 2);
+        assertEquals(lcache.size(), 4);
+        assertEquals(lcache.getValueForKey(1), 1);
+        assertEquals(lcache.getValueForKey(4), 4);
+        assertEquals(lcache.getValueForKey(3), 3);
+        assertEquals(lcache.getValueForKey(2), 2);
     }
 
-    @Test
-    public void TopError() {
-        LruCache<Integer, Integer> lcache = new LruCache<>();
-        assertThrows(AssertionError.class, () -> System.out.println(lcache.getTopKey()));
-    }
+
 
     @Test
     public void WrongSetSize() {
